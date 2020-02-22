c6243e8c65f480f4bbb97dc1e6f5b827fa9a99ae
jyoti <jyoti@DESKTOP-5AEFB7S>
2020-02-22 12:52:53 +0530

    update

--- src/com/jyoti/test/StringWithPredefind.java
+++ src/com/jyoti/test/StringWithPredefind.java
@@ -2,7 +2,7 @@ package com.jyoti.test;
 
 public class StringWithPredefind {
 	public static void main(String[] args) {
-		 String name="raj kumar";
+		 String name="jyoti kumar";
 		 StringBuffer s1=new StringBuffer().append(name);
 		 s1.reverse();
 		 System.out.println(s1);
